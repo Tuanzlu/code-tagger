6f96a536e45905a6095a28399ec193b423ccbad7
a1772438159 <50148344+a1772438159@users.noreply.github.com>
2022-12-22 16:11:51 +0800

    Update user_db.py

--- back-end/pythonProject/db/user_db.py
+++ back-end/pythonProject/db/user_db.py
@@ -6,23 +6,34 @@ class UserDB(DB):
     def __init__(self, db_path='data/user.sqlite'):
         self.db_path = db_path
         self.init_table_sql = '''
-        CREATE TABLE IF NOT EXISTS "users" ("_id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "username" VARCHAR, "passwd" VARCHAR);
+        CREATE TABLE IF NOT EXISTS "users" ("_id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "username" VARCHAR, "passwd" VARCHAR, "telphone" VARCHAR);
         '''
         super(UserDB, self).__init__(self.db_path, self.init_table_sql)
 
-    def addUser(self, user, passwd):
-        self.execute("INSERT INTO users (username,passwd) VALUES (?,?)", (user, passwd))
+    def addUser(self, username, passwd, telphone):
+        self.execute("INSERT INTO users (username,passwd,telphone) VALUES (?,?,?)", (username, passwd, telphone))
         return self.cursor.lastrowid
 
     def selectAllUser(self):
         self.cursor.execute("SELECT * FROM users")
         return self.cursor.fetchall()
 
+    def removeUser(self, username, passwd, telphone):
+        self.execute("DELETE FROM users WHERE user='{}' AND passwd='{}' AND telphone='{}'".format(username, passwd, telphone))
+
+    def GetSameInfo(self, telphone):
+        self.cursor.execute("SELECT * FROM users WHERE telphone='{}'".format(telphone))
+        return self.cursor.fetchone()
+
+    def GetLoginInfo(self, telphone, passwd):
+        self.cursor.execute("SELECT passwd FROM users WHERE telphone='{}'".format(telphone))
+        return self.cursor.fetchone()
+
 
 if __name__ == "__main__":
     userdb = UserDB()
     print("first print...")
     print(userdb.selectAllUser())
-    userdb.addUser("admin", "123456")
+    userdb.addUser("admin", "123456", "11111111111")
     print("second print...")
     print(userdb.selectAllUser())
