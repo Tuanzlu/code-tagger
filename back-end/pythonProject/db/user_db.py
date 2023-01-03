0d980ba522b7569f1d279d8b833f7be0923434c2
a1772438159 <50148344+a1772438159@users.noreply.github.com>
2023-01-03 13:02:38 +0800

    Update user_db.py

--- back-end/pythonProject/db/user_db.py
+++ back-end/pythonProject/db/user_db.py
@@ -2,13 +2,13 @@
 from db.sqlite3_db import DB
 
 
-class UserDB(DB):
+class user_DB(DB):
     def __init__(self, db_path='data/user.sqlite'):
         self.db_path = db_path
         self.init_table_sql = '''
         CREATE TABLE IF NOT EXISTS "users" ("_id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "username" VARCHAR, "passwd" VARCHAR, "telphone" VARCHAR);
         '''
-        super(UserDB, self).__init__(self.db_path, self.init_table_sql)
+        super(user_DB, self).__init__(self.db_path, self.init_table_sql)
 
     def addUser(self, username, passwd, telphone):
         self.execute("INSERT INTO users (username,passwd,telphone) VALUES (?,?,?)", (username, passwd, telphone))
@@ -31,7 +31,7 @@ class UserDB(DB):
 
 
 if __name__ == "__main__":
-    userdb = UserDB()
+    userdb = user_DB()
     print("first print...")
     print(userdb.selectAllUser())
     userdb.addUser("admin", "123456", "11111111111")
