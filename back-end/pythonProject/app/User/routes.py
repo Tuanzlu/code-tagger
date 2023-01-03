986bc9a65fbe3f2107ba2f2e430a239b1aa881c9
a1772438159 <50148344+a1772438159@users.noreply.github.com>
2023-01-03 21:41:33 +0800

    Update routes.py

--- back-end/pythonProject/app/User/routes.py
+++ back-end/pythonProject/app/User/routes.py
@@ -33,40 +33,52 @@ def hello_world():
 @User.route('/login', methods=['POST'])
 def login():
     info = ''
-    telphone = request.form['telphone']
-    password = request.form['password']
+    telphone = request.form.get("telphone")
+    password = request.form.get("password")
     res = user.GetLoginInfo(telphone)
     if res == None:
         info = '无用户信息，请注册'
-        return render_template('index.html', info=info)
-    elif res[0] == password:
+        #return render_template('index.html', info=info)
+        return '无用户信息，请注册'
+    temp = list(res.values())
+    res_p = temp[0]
+    if res_p == password:
         info = '登录成功'
         session['telphone'] = telphone
-        return render_template('index.html', info=info)
+        #return render_template('index.html', info=info)
+        return '登录成功'
     else:
         info = '登录失败'
-        return render_template('index.html', info=info)
+        #return render_template('index.html', info=info)
+        return '登录失败'
+
+
     return render_template('index.html', info='Unknow Error!!!')
 
 
 @User.route('/register', methods=['POST'])
 def register():
     info = ''
-    username = request.form['username']
-    telphone = request.form['telphone']
-    password = request.form['password']
-    password_again = request.form['password_again']
+    username = request.form.get("username")
+    telphone = request.form.get("telphone")
+    password = request.form.get("password")
+    password_again = request.form.get("password_again")
     res = user.GetSameInfo(telphone)
     if res:
         info = '手机号已被使用，请更换手机号重试'
-        return render_template('index.html', info=info)
+        #return render_template('index.html', info=info)
+        return '手机号已被使用，请更换手机号重试'
     elif password != password_again:
         info = '两次密码不一致'
-        return render_template('index.html', info=info)
+        #return render_template('index.html', info=info)
+        return '两次密码不一致'
     else:
         info = '注册成功'
         user.addUser(username, password, telphone)
-        return render_template('index.html', info=info)
+        # return jsonify({"state": 'success', "description": "success"})
+        # return render_template('index.html', info=info)
+        return '注册成功'
+
     return render_template('index.html', info='Unknow Error!!!')
 
 
@@ -76,10 +88,37 @@ def exit():
     res = session.get('telphone')
     if res == None:
         info = '未登录'
-        return render_template('index.html', info=info)
+        #return render_template('index.html', info=info)
+        return '未登录'
     else:
         session['telphone'] = None
         info = '账号已成功注销'
-        return render_template('index.html', info=info)
+        #return render_template('index.html', info=info)
+        return '账号已成功注销'
+
     return render_template('index.html', info='Unknow Error!!!')
 
+@User.route('/removeUser', methods=["POST", "GET"])
+def removeUser():
+    info = ''
+    if request.method == "GET":
+        username = request.args.get("username")
+        telphone = request.args.get("telphone")
+        password = request.args.get("password")
+    else:
+        username = request.form.get("username")
+        telphone = request.form.get("telphone")
+        password = request.form.get("password")
+    res = user.checkUser(username, password, telphone)
+    if res == None:
+        info = '输入信息有误，删除失败'
+        #return render_template('index.html', info=info)
+        return '输入信息有误，删除失败'
+    else:
+        info = '删除成功'
+        user.removeUser(username, password, telphone)
+        # return render_template('index.html', info=info)
+        # return jsonify({"state":'success', "description": "success"})
+        return '删除成功'
+
+    return render_template('index.html', info='Unknow Error!!!')
