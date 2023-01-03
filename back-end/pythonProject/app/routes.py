1a833b0e1c3d9d3840855048941e3b197f847005
a1772438159 <50148344+a1772438159@users.noreply.github.com>
2023-01-03 13:00:58 +0800

    Update routes.py

--- back-end/pythonProject/app/routes.py
+++ back-end/pythonProject/app/routes.py
@@ -1,4 +1,8 @@
 from app import app
-from app.UserRepository import UserRepository
+from app.UserLabel import UserLabel
+from app.Code import Code
+from app.User import User
 
-app.register_blueprint(UserRepository,url_prefix='/UserRepository')
+app.register_blueprint(UserLabel,url_prefix='/UserLabel')
+app.register_blueprint(Code,url_prefix='/Code')
+app.register_blueprint(User,url_prefix='/User')
