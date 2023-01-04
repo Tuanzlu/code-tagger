bb83d1889f511b967b8b593b52a530cc32ecda4a
xyxr <73784441+xyxr@users.noreply.github.com>
2023-01-04 21:05:29 +0800

    Update routes.py

--- back-end/pythonProject/app/UserLabel/routes.py
+++ back-end/pythonProject/app/UserLabel/routes.py
@@ -2,6 +2,7 @@ from app.UserLabel import UserLabel
 
 from flask import request, session, jsonify
 from db.label_db import label_DB
+from db.mark_db import mark_DB
 
 # 两个函数，用于将flask_sql的输出转化为可以直接网络传输的json格式
 # 用法：直接调用classlist2dictlist，输入可以是单个object或者多个object的列表
@@ -37,19 +38,39 @@ def addUserLabel_func():
         label = request.form.get("label")
     label1 = label_DB()
     rst = label1.oneUserAddLabel(userId, labelId, label)
-    return jsonify({"state":'success', "description": "success", "moreMsg":rst})
-
+    if rst == "existed":
+        return jsonify({"state":'fail', "description": "Existing label", "moreMsg":rst})
+    else:
+        return jsonify({"state":'success', "description": "success", "moreMsg":rst})
 
 @UserLabel.route('/getLabel', methods=["POST", "GET"])
-def getUserLabel_func():
+def getLabel_func():
     if request.method == "GET":
         userId = request.args.get("userId")
+        labelId = request.args.get("labelId")
     else:
         userId = request.form.get("userId")
+        labelId = request.form.get("labelId")
     print(userId)
     label1 = label_DB()
-    rst = label1.selectOneUser(userId)
+    rst = label1.checkUserLabel(userId,labelId)
     return jsonify({'state':'success', "rst":rst})
+
+@UserLabel.route('/getUser', methods=["POST", "GET"])
+def getUserUser_func():
+    if request.method == "GET":
+        userId = request.args.get("userId")
+        detail = request.args.get("detail")
+    else:
+        userId = request.form.get("userId")
+        detail = request.form.get("detail")
+    label1 = label_DB()
+    if detail == False:
+        rst = label1.selectOneUserlist(userId) 
+    else:
+        rst = label1.selectOneUserdetail(userId) 
+    return jsonify({'state':'success', "rst":rst})
+
 
 @UserLabel.route('/removeLabel', methods=["POST", "GET"])
 def removeUserLabel_func():
@@ -68,14 +89,20 @@ def modifyLabelID_func():
     if request.method == "GET":
         userId = request.args.get("userId")
         labelId = request.args.get("labelId")
-        labelID_new = request.args.get("labelId_new")
+        labelId_new = request.args.get("labelId_new")
     else:
         userId = request.form.get("userId")
         labelId = request.form.get("labelId")
-        labelID_new = request.args.get("labelId_new")
+        labelId_new = request.form.get("labelId_new")
     label1 = label_DB()
-    label1.oneUserModifyLabelID(userId, labelId, labelID_new)
-    return jsonify({"state":'success', "description": "success"})
+    mark1 = mark_DB()
+    rst = label1.checkUserLabel(userId,labelId_new)
+    if rst == "existed":
+        return jsonify({"state":'fail', "description": "Existing label"})
+    else:
+        label1.oneUserModifyLabelID(userId, labelId, labelId_new)
+        mark1.oneUserModifyLabelID(userId, labelId, labelId_new)
+        return jsonify({"state":'success', "description": "success"})
 
 @UserLabel.route('/modifyLabelintro', methods=["POST", "GET"])
 def modifyLabelintro_func():
@@ -86,7 +113,7 @@ def modifyLabelintro_func():
     else:
         userId = request.form.get("userId")
         labelId = request.form.get("labelId")
-        label_new = request.args.get("label_new")
+        label_new = request.form.get("label_new")
     label1 = label_DB()
     label1.oneUserModifyLabelintro(userId, labelId, label_new)
     return jsonify({"state":'success', "description": "success"})
