28352841e16c9eb294c7384ce35c5bb4aee01dce
Shawn <yuanxinyi@mails.ucas.ac.cn>
2023-01-05 21:53:27 +0800

    modify router/index.js

--- front-end/code-tagger/src/router/index.js
+++ front-end/code-tagger/src/router/index.js
@@ -20,37 +20,37 @@ const routes = [
     component: () => import("../views/Intro.vue"),
   },
   {
-    // 平台使用说明
+    // 在线代码编辑器
     path: "/editor",
     name: "editor",
     component: () => import("../views/Editor.vue"),
   },
   {
-    // 平台使用说明
+    // 个人信息
     path: "/person",
     name: "person",
     component: () => import("../views/PersonalIndex.vue"),
   },
   {
-    // 平台使用说明
+    // 用户注册
     path: "/register",
     name: "register",
     component: () => import("../views/Register.vue"),
   },
   {
-    // 平台使用说明
+    // 标注关系库
     path: "/relation",
     name: "relation",
     component: () => import("../views/RelationManage.vue"),
   },
   {
-    // 平台使用说明
+    // 标签库
     path: "/tag",
     name: "tag",
     component: () => import("../views/TagManage.vue"),
   },
   {
-    // 平台使用说明
+    // 代码库
     path: "/code",
     name: "code",
     component: () => import("../views/CodeManage.vue"),
