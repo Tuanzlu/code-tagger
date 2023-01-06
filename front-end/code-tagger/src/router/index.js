53214a803559b71e60b19eb0de3d04d6d90c94e0
Shawn <yuanxinyi@mails.ucas.ac.cn>
2023-01-06 12:35:33 +0800

    fix bugs

--- front-end/code-tagger/src/router/index.js
+++ front-end/code-tagger/src/router/index.js
@@ -8,16 +8,22 @@ const routes = [
     component: () => import("../views/Index.vue"),
   },
   {
-    // 用户登录 未启用
+    // 平台使用说明
+    path: "/intro",
+    name: "intro",
+    component: () => import("../views/Intro.vue"),
+  },
+  {
+    // 登录
     path: "/login",
     name: "login",
     component: () => import("../views/Login.vue"),
   },
   {
-    // 平台使用说明
-    path: "/intro",
-    name: "intro",
-    component: () => import("../views/Intro.vue"),
+    // 注册
+    path: "/register",
+    name: "register",
+    component: () => import("../views/Register.vue"),
   },
   {
     // 在线代码编辑器
@@ -26,22 +32,16 @@ const routes = [
     component: () => import("../views/Editor.vue"),
   },
   {
-    // 个人信息
-    path: "/person",
-    name: "person",
+    // 代码库+标签库目录
+    path: "/index",
+    name: "index",
     component: () => import("../views/PersonalIndex.vue"),
   },
   {
-    // 用户注册
-    path: "/register",
-    name: "register",
-    component: () => import("../views/Register.vue"),
-  },
-  {
-    // 标注关系库
-    path: "/relation",
-    name: "relation",
-    component: () => import("../views/RelationManage.vue"),
+    // 代码库
+    path: "/code",
+    name: "code",
+    component: () => import("../views/CodeManage.vue"),
   },
   {
     // 标签库
@@ -50,10 +50,10 @@ const routes = [
     component: () => import("../views/TagManage.vue"),
   },
   {
-    // 代码库
-    path: "/code",
-    name: "code",
-    component: () => import("../views/CodeManage.vue"),
+    // 标注关系库
+    path: "/relation",
+    name: "relation",
+    component: () => import("../views/RelationManage.vue"),
   },
 ];
 
