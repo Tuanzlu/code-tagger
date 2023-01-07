634ce3354ab9298fecc89226904504b36c239830
xyxr <73784441+xyxr@users.noreply.github.com>
2023-01-07 17:40:48 +0800

    Update code_db.py
    新建或修改代码文件时存储时间戳

--- back-end/pythonProject/db/code_db.py
+++ back-end/pythonProject/db/code_db.py
@@ -1,18 +1,19 @@
 # 调用自定义包: db, 具体路径在run.py同目录下的文件夹中, 操作数据库
 # print(__main__)
 from db.sqlite3_db import DB
+from datatime.get_time import get_now_time
 # from db.sqlite3_db import DB
 
 class code_DB(DB):
     def __init__(self, db_path = 'data/code.sqlite'):
         self.db_path = db_path
         self.init_table_sql = '''
-        CREATE TABLE IF NOT EXISTS "Code" ("_id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "userID" VARCHAR, "code" VARCHAR, "code_name" VARCHAR);
+        CREATE TABLE IF NOT EXISTS "Code" ("_id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "userID" VARCHAR, "code" VARCHAR, "code_name" VARCHAR, "time" VARCHAR);
         '''
         super(code_DB, self).__init__(self.db_path, self.init_table_sql)
     
     def selectOneUser(self, userID):
-        self.cursor.execute("SELECT userID, code, code_name FROM Code WHERE userID = '{}'".format(userID))
+        self.cursor.execute("SELECT userID, code, code_name, time FROM Code WHERE userID = '{}'".format(userID))
         outs = self.cursor.fetchall()
         return outs
 
@@ -25,8 +26,9 @@ class code_DB(DB):
 
     def oneUserAddCode(self, userID, c, c_name):
         check_rst = self.checkUserCode(userID, c_name)
+        t = get_now_time()
         if not check_rst:
-            self.execute("INSERT INTO Code (userID,code,code_name) VALUES (?,?,?)",(userID, c, c_name))
+            self.execute("INSERT INTO Code (userID,code,code_name,time) VALUES (?,?,?,?)",(userID, c, c_name,t))
             return self.cursor.lastrowid
         else:
             return "existed"
@@ -44,11 +46,13 @@ class code_DB(DB):
             return "existed"
 
     def oneUserModifyCode(self, userID, c_name, c_new):
+        t = get_now_time()
         self.execute("UPDATE Code SET code='{}' WHERE userID='{}' AND code_name='{}'".format(c_new, userID, c_name))
+        self.execute("UPDATE Code SET time='{}' WHERE userID='{}' AND code_name='{}'".format(t, userID, c_name))
         return self.cursor.lastrowid
 
     def selectOneCode(self, userID,c_name):
-        self.cursor.execute("SELECT userID, code, code_name FROM Code WHERE userID = '{}' AND code_name='{}'".format(userID,c_name))
+        self.cursor.execute("SELECT userID, code, code_name, time FROM Code WHERE userID = '{}' AND code_name='{}'".format(userID,c_name))
         outs = self.cursor.fetchall()
         return outs
 
@@ -66,5 +70,7 @@ if __name__ == "__main__":
     print(code1.checkUserCode("admin", "baidu"))
     #print(code1.checkUserCode("admin",'www.360.com',"baidu"))
     print(code1.selectOneUser("admin"))
+    
+    
     
     
