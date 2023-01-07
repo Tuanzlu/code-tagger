5269a8de3683f818fe46fc325a133f9fe87bfd63
xyxr <73784441+xyxr@users.noreply.github.com>
2023-01-07 17:36:56 +0800

    Update get_time.py
    获得当前时间的正则表达式数据

--- back-end/pythonProject/datatime/get_time.py
+++ back-end/pythonProject/datatime/get_time.py
@@ -1,7 +1,10 @@
 import time
+from datetime import datetime
 
 def get_now_time():
-    return str(time.time())
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+    return dt_string
 
 if __name__ == "__main__":
     print(get_now_time())
