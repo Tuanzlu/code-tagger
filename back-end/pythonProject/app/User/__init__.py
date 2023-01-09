d4a604b6b7bf37f290d785465ea50b4583994c3e
a1772438159 <50148344+a1772438159@users.noreply.github.com>
2023-01-09 18:37:49 +0800

    Create __init__.py

--- back-end/pythonProject/app/User/__init__.py
+++ back-end/pythonProject/app/User/__init__.py
@@ -1,5 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 User = Blueprint("User", __name__)
 
 from app.User import routes
+
+from db.user_db import user_DB
+
+user = user_DB()
+
+res = user.GetSameUsername('admin')
+if res == None:
+    user.addUser('admin', '123456', '11111111111')
