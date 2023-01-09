080f1f1b5fe1dcf551ef67cfbd499f6aba4bc833
xyxr <73784441+xyxr@users.noreply.github.com>
2023-01-09 17:12:38 +0800

    Update routes.py

--- back-end/pythonProject/app/routes.py
+++ back-end/pythonProject/app/routes.py
@@ -3,8 +3,11 @@ from app.UserLabel import UserLabel
 from app.Code import Code
 from app.User import User
 from app.Mark import Mark
+from app.Admin import Admin
+
 
 app.register_blueprint(UserLabel,url_prefix='/UserLabel')
 app.register_blueprint(Code,url_prefix='/Code')
 app.register_blueprint(User,url_prefix='/User')
 app.register_blueprint(Mark,url_prefix='/Mark') 
+app.register_blueprint(Admin,url_prefix='/Admin') 
